a8d8ab4f0bef2f2ca0b80ba926db160523fcee19
kangsinbeom <kanian16@naver.com>
2024-04-25 22:24:52 +0900

    공통컴포넌트 생성(icon, shadowbox, text)
    text는 아직 제대로 구현된 사항이 없으며 typography 부분을 정의한  뒤 사용 예정

---
+++ src/components/shared/Text.tsx
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+const Text = styled.p``;
+
+export default Text;
---
+++ src/components/shared/index.ts
@@ -0,0 +1,3 @@
+export { default as Text } from './Text';
+export { default as ShadowBox } from './ShadowBox';
+export { default as Icon } from './Icon';
