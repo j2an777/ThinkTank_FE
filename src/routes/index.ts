925ead8ad5fc8e62da4f6e225812be249960f2c7
kangsinbeom <kanian16@naver.com>
2024-04-25 22:20:24 +0900

    feat: 글쓰기 router 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
-import { LoginPage, MainPage } from '../pages';
+import { LoginPage, MainPage, PostPage } from '../pages';
 
 export const routers = [
-  { path: 'login', component: LoginPage },
+  { path: '/login', component: LoginPage },
   { path: '/', component: MainPage },
+  { path: '/post', component: PostPage },
 ];
