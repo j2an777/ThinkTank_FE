54110d2373998237dc5b36af566fedb40b8fdf90
soohyun <gamjatan9@gmail.com>
2024-04-26 22:57:14 +0900

    Fix: 코드 스타일 변경
    코드 스타일 통일하기 위해 수정했습니다.

--- src/components/shared/authBackground/index.tsx
+++ src/components/shared/authBackground/index.tsx
@@ -1,10 +1,12 @@
 import * as S from './styles';
 
-export default function AuthBackground() {
+const AuthBackground = () => {
   return (
     <>
       <S.Circle x={-20} y={-25}></S.Circle>
       <S.Circle x={65} y={50}></S.Circle>
     </>
   );
-}
+};
+
+export default AuthBackground;
---
+++ src/components/shared/index.ts
@@ -0,0 +1,2 @@
+export { default as AuthBackground } from './authBackground';
+export { default as InputBox } from './inputBox';
