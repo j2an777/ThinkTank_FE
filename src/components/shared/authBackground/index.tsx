d98051ec8c792c578991771aa00b0392d4663f6b
soohyun <gamjatan9@gmail.com>
2024-04-26 17:10:07 +0900

    Refactor: 로그인 및 회원가입 배경component 분리
    기존에 signupPage에 있던 배경 관련 코드를 따로 컴포넌트로 만들어 분리했습니다.

---
+++ src/components/shared/authBackground/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+
+export default function AuthBackground() {
+  return (
+    <>
+      <S.Circle x={-20} y={-25}></S.Circle>
+      <S.Circle x={65} y={50}></S.Circle>
+    </>
+  );
+}
--- src/components/shared/authBackground/styles.ts
+++ src/components/shared/authBackground/styles.ts
@@ -19,15 +19,3 @@ export const Circle = styled.div<{ x: number; y: number }>`
   border-radius: 100em;
   background: ${colors.yellow50};
 `;
-
-// 임시 --> 추후 삭제
-export const ShadowBox = styled.div`
-  width: 940px;
-  height: 1000px;
-  border-radius: 30px;
-  background: #fff;
-  box-shadow: 0px 0px 30px 0px rgba(106, 106, 106, 0.15);
-  z-index: 1;
-  display: flex;
-  justify-content: center;
-`;
