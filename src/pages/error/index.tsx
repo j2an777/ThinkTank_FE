cc927130661ea52bece4c10a29327ebfdb9519fa
j2an777 <ha991104@gmail.com>
2024-04-29 00:59:01 +0900

    Feat: 404 페이지 제작

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
+import { ErrorBottomBox, ErrorContainer, ErrorContentBlock, ErrorPBlock, ErrorTopBox } from "./styles"
+import Error from '../../assets/images/404.gif';
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <ErrorContainer>
+        <ErrorTopBox>
+            <ErrorPBlock>4</ErrorPBlock>
+            <img src={Error} />
+            <ErrorPBlock>4</ErrorPBlock>
+        </ErrorTopBox>
+        <ErrorBottomBox>
+            <ErrorContentBlock>Page not found.</ErrorContentBlock>
+            <Link to="/">Home</Link>
+        </ErrorBottomBox>
+    </ErrorContainer>
   )
 }
 
