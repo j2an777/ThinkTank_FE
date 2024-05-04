b78db6fb00d8bcf3f0393558e790ee3af57b1768
j2an777 <ha991104@gmail.com>
2024-05-04 17:50:11 +0900

    [Feat: devtool] 리액트쿼리 devtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { Global } from '@emotion/react';
 import globalStyles from './styles/globalStyles.ts';
 import { ModalContextProvider } from './contexts/ModalContext.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ModalContextProvider>
         <App />
       </ModalContextProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
