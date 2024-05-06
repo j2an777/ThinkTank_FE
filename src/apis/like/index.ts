1357ec39dd771bd0bdfc5d1555e76de5fc7cd48f
j2an777 <ha991104@gmail.com>
2024-05-06 23:23:21 +0900

    [Fix: AuthApi] baseUrl, header 통합 컴포넌트 적용

--- src/apis/like/index.ts
+++ src/apis/like/index.ts
@@ -1,18 +1,9 @@
-import axios from "axios";
-
-
-const accessToken = localStorage.getItem('access_token');
+import { AuthApi } from "../AuthAPI";
 
 // 좋아요 수 업데이트 구문
 const updateLike = async (postId: number): Promise<void> => {
     try {
-        const response = await axios.post('/api/like', {
-            postId
-        }, {
-            headers: {
-                Authorization: `Bearer as ${accessToken}`,
-            }
-        });
+        const response = await AuthApi.post('/api/like', postId);
         console.log('성공', response);
     } catch (error) {
         console.error('실패', error);
