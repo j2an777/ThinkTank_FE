7b0841e769bc3c716c50d1b451d23b560b782b65
soohyun <gamjatan9@gmail.com>
2024-05-06 22:08:41 +0900

    Feat: user info api 연동

--- src/components/mypage/userInfo/index.tsx
+++ src/components/mypage/userInfo/index.tsx
@@ -1,19 +1,46 @@
+import { UserCircle } from '@/components/shared/index.ts';
 import * as S from './styles.ts';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getMyPage } from '@/apis/mypage.ts';
+import { User } from '@/types/auth.ts';
 
 const UserInfo = () => {
+  const [userData, setUserData] = useState<User>({
+    nickname: 'Soohyun Kim',
+    profileImage: null,
+    email: 'soo@gmail.com',
+    introduce: 'hello',
+  });
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const data = await getMyPage();
+      if (data) {
+        setUserData({
+          nickname: data.nickname,
+          profileImage: data.profileImage,
+          email: data.email,
+          introduce: data.introduce,
+        });
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <S.Container>
       <S.LeftBox>
-        <S.ProfileImage></S.ProfileImage>
+        <UserCircle size={150} profileImage={userData.profileImage} />
       </S.LeftBox>
       <S.RightBox>
-        <S.Edit>편집</S.Edit>
-        <S.UserName>soohyun Kim</S.UserName>
-        <S.UserEmail>soo@gmail.com</S.UserEmail>
-        <S.UserIntro>
-          hello hello hello hello hello hello hello hello hello hello hello hello hello
-          hello hello hello hello hello hello hello hello hello hello hello hello hello
-        </S.UserIntro>
+        <Link to="profile">
+          <S.Edit>편집</S.Edit>
+        </Link>
+        <S.UserName>{userData.nickname}</S.UserName>
+        <S.UserEmail>{userData.email}</S.UserEmail>
+        <S.UserIntro>{userData.introduce}</S.UserIntro>
       </S.RightBox>
     </S.Container>
   );
