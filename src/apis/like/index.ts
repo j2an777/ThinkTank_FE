67cedad17bc26db486973c346aba126150fe2ab1
kangsinbeom <kanian16@naver.com>
2024-05-08 00:56:59 +0900

    Fix: api 수정

--- src/apis/like/index.ts
+++ src/apis/like/index.ts
@@ -1,13 +1,22 @@
-import { AuthApi } from "../AuthAPI";
+import { getAuthAxios } from '../authAxios';
 
 // 좋아요 수 업데이트 구문
 const updateLike = async (postId: number): Promise<void> => {
-    try {
-        const response = await AuthApi.post('/api/like', postId);
-        console.log('성공', response);
-    } catch (error) {
-        console.error('실패', error);
-    }
+  const access = localStorage.getItem('access');
+
+  if (!access) {
+    console.error('Access token is missing or invalid');
+    throw new Error('Access token is missing or invalid');
+  }
+
+  const authAxios = getAuthAxios(access);
+
+  try {
+    const response = await authAxios.post('/api/like', postId);
+    console.log('성공', response);
+  } catch (error) {
+    console.error('실패', error);
+  }
 };
 
 export default updateLike;
--- src/apis/main/index.ts
+++ src/apis/main/index.ts
@@ -1,19 +1,30 @@
-import { List } from "@/types/article";
-import { AuthApi } from "../AuthAPI";
+import { List } from '@/types/article';
+import { getAuthAxios } from '../authAxios';
 
 interface FetchParams {
-    pageParam?: number;
-    limit : number;
+  pageParam?: number;
+  limit: number;
 }
 
 const fetchPosts = async ({ pageParam = 1, limit = 2 }: FetchParams): Promise<List> => {
-    try {
-        const response = await AuthApi.get<List>(`https://baa4852a-d5cb-4d7d-9716-715686200726.mock.pstmn.io/api/posts?page=${pageParam}&limit=${limit}`);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw new Error('Failed to fetch posts');
-    }
+  const access = localStorage.getItem('access');
+
+  if (!access) {
+    console.error('Access token is missing or invalid');
+    throw new Error('Access token is missing or invalid');
+  }
+
+  const authAxios = getAuthAxios(access);
+
+  try {
+    const response = await authAxios.get<List>(
+      `https://baa4852a-d5cb-4d7d-9716-715686200726.mock.pstmn.io/api/posts?page=${pageParam}&limit=${limit}`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to fetch posts');
+  }
 };
 
 export default fetchPosts;
