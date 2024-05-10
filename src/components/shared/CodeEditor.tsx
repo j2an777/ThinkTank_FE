f1dcbd6b12941b0e02944d5c60685e7ec3a034ae
kangsinbeom <kanian16@naver.com>
2024-05-10 09:48:41 +0900

    Fix:코드 에디터 readonly 기능 추가

--- src/components/shared/CodeEditor.tsx
+++ src/components/shared/CodeEditor.tsx
@@ -1,9 +1,13 @@
+import { DEFALUTCODE } from '@/consts/defaultCode';
 import { LanguageValues } from '@/consts/language';
 import useSetFormData from '@/hooks/post/useSetFormData';
+import getCodeEditorOptions from '@/utils/getCodeEditorOptions';
 import Editor, { loader } from '@monaco-editor/react';
 
 interface CodeEditorProps {
   language: LanguageValues;
+  readOnly?: boolean;
+  answer?: string;
 }
 
 loader.config({
@@ -24,25 +28,26 @@ loader.init().then((monaco) => {
 });
 // colors 관련 key 정보 https://github.com/microsoft/monaco-editor/issues/1631
 
-const CodeEditor = ({ language }: CodeEditorProps) => {
+const CodeEditor = ({ language, readOnly = false, answer }: CodeEditorProps) => {
   const { updatePostForm, postForm } = useSetFormData();
+  const options = getCodeEditorOptions({ readOnly });
+  console.log(postForm);
   return (
     <Editor
       height="400px"
-      options={{
-        readOnly: false,
-        minimap: { enabled: false },
-        scrollbar: { vertical: 'hidden' },
-        fontSize: 12,
-      }}
+      options={options}
       defaultLanguage="java"
       language={language}
-      defaultValue="// some comment"
-      value={postForm.answer}
-      onChange={(value) => updatePostForm({ answer: value })}
+      defaultValue={DEFALUTCODE.java}
+      value={answer ?? postForm.answer}
+      onChange={(answer) => updatePostForm({ answer })}
       theme="myTheme"
     />
   );
 };
 
 export default CodeEditor;
+
+/**
+ * 호출 시에 readOnly 속성만 주시고
+ */
---
+++ src/consts/defaultCode.ts
@@ -0,0 +1,14 @@
+export const DEFALUTCODE = {
+  java: [
+    'import java.io.*;',
+    'import java.util.*;',
+    'import java.util.stream.*;',
+    '',
+    'class ThinkTank {',
+    '    public static String solution(String str) {',
+    '        String answer = "";',
+    '        return answer;',
+    '    }',
+    '}',
+  ].join('\n'),
+};
