b271b0734e5820d3c80f2e8ced32346a0f8a2318
j2an777 <ha991104@gmail.com>
2024-05-16 11:20:51 +0900

    Feat: 보호된 라우트
    보호된 라우트 적용 1차 완료

---
+++ src/routes/protectedRoute.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from "react";
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  element: ReactElement;
+}
+
+// 보호된 라우트 완성짓기
+const ProtectedRoute = ({ element }: ProtectedRouteProps) => {
+  const accessToken = localStorage.getItem('accessToken');
+
+  // accessToken이 없다면 로그인 페이지로 리디렉트
+  return accessToken ? element : <Navigate to="/login" replace={true} />;
+};
+
+export default ProtectedRoute;
