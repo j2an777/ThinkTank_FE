6cf078e699a16c779d86640c95dbc07d8e868fd6
soohyun <gamjatan9@gmail.com>
2024-05-20 10:34:29 +0900

    Fix: 회원가입 수정

--- src/pages/signup/optional/index.tsx
+++ src/pages/signup/optional/index.tsx
@@ -1,17 +1,13 @@
 import { AuthBackground, ShadowBox } from '@/components/shared';
 import * as S from '@/components/shared/authBackground/styles.ts';
 import OptionalForm from '@/components/signup/optionalForm';
-import { useLocation } from 'react-router-dom';
 
 const SignupOptionalPage = () => {
-  const location = useLocation();
-  const { nickname } = location.state || { nickname: '' };
-
   return (
     <S.Container>
       <AuthBackground />
       <ShadowBox padding={50} height={850}>
-        <OptionalForm nickname={nickname} />
+        <OptionalForm />
       </ShadowBox>
     </S.Container>
   );
