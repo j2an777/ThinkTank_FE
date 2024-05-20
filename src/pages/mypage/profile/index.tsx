6f5a5f947728ed7b8f9d50e008c7b6ed5d3461c8
soohyun <gamjatan9@gmail.com>
2024-05-20 10:32:59 +0900

    Fix: 프로필 페이지 수정

--- src/pages/mypage/profile/index.tsx
+++ src/pages/mypage/profile/index.tsx
@@ -3,12 +3,31 @@ import * as S from './styles';
 import { Icon } from '@/components/shared';
 import { useNavigate } from 'react-router-dom';
 import { removeAccess } from '@/hooks/auth/useLocalStorage';
+import { deleteUser } from '@/apis/user';
+import { useModalContext } from '@/contexts/ModalContext';
 
 const Profile = () => {
   const navigate = useNavigate();
+  const { open } = useModalContext();
+
   const logout = () => {
     removeAccess();
     navigate('/');
+  };
+
+  const onDelete = () => {
+    open({
+      title: '회원탈퇴',
+      description:
+        '정말 탈퇴하시겠습니까? 탈퇴 후 회원정보 및 개인형 서비스 이용기록은 모두 삭제됩니다.',
+      onButtonClick: () => {
+        deleteUser();
+        removeAccess();
+        navigate('/');
+      },
+      hasCancelButton: true,
+      buttonLabel: '확인',
+    });
   };
 
   return (
@@ -17,7 +36,9 @@ const Profile = () => {
         <S.Title>프로필 수정</S.Title>
         <S.Subtitle>대표 프로필 사진과 별명을 수정 하실 수 있습니다.</S.Subtitle>
       </S.Block>
-      <ProfileTable />
+      <S.Block>
+        <ProfileTable />
+      </S.Block>
       <S.Block>
         <S.Box>
           <S.Logout>로그아웃</S.Logout>
@@ -27,7 +48,7 @@ const Profile = () => {
       <S.Block>
         <S.Box>
           <S.Title>회원탈퇴</S.Title>
-          <Icon value="quit" onClick={logout} />
+          <Icon value="quit" onClick={onDelete} />
         </S.Box>
         <S.Subtitle>
           탈퇴 후 회원정보 및 개인형 서비스 이용기록은 모두 삭제됩니다. 탈퇴한 아이디는
